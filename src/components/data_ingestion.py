358e61682ed6240b22ddbe0b6adea024bfe2efcc
Parshva Karani <parshavkarani26@gmail.com>
2025-09-19 12:01:36 +0530

    Data ingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,8 +24,29 @@ class DataIngestion:
         try:
             df = pd.read_csv('Notebook\data\stud.csv')
             logging.info("Read the dataset as dataframe")
-            os.makedir(os.path.dirname(self.ingestion_config.train_data_path), exist_ok =True)
-            df.to_csv(self.ingestion_config.raw_Data_path, index=False, header=True)
-        except:
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok =True)
+
             
-            pass
+            df.to_csv(self.ingestion_config.raw_Data_path, index=False, header=True)
+            logging.info("Train Test Split Initiated")
+            
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header =True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header =True)
+
+            logging.info("Ingestion of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+if __name__ =="__main__":
+    obj =DataIngestion()
+    obj.initiate_data_ingestion()
---
+++ src/components/data_transformation.py
@@ -0,0 +1,24 @@
+import sys
+from dataclasses import dataclass
+
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipelinefrom 
+from sklearn.preprocessing import OnehotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os 
+
+@dataclass
+class DataTransformationConfig():
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    
